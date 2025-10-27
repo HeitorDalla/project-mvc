b9a77631c58c31a16c28f15165181777c1c4258b
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-27 20:28:52 -0300

    Modificando a chave estrangeira do JPA na tabela de usuario

--- src/main/java/br/edu/unicesumar/model/Usuario.java
+++ src/main/java/br/edu/unicesumar/model/Usuario.java
@@ -5,7 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+
+import jakarta.persistence.CascadeType;
 
 //INFORMANDO PARA O JPA QUE ESTE OBJETO É UMA TABELA NO BANCO
 @Entity
@@ -26,37 +30,43 @@ public class Usuario {
     @Column(name="email_usuario", nullable = false)
     private String email;
 
-    // CONSTRUTORES
-    public Usuario(){}
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_endereco_usuario", referencedColumnName = "id_endereco")
+    private Endereco endereco;
 
-    public Usuario(int id, String nome, String cpf) {
+    // CONSTRUTORES
+    public Usuario () {}
+
+    public Usuario(int id, String nome, String cpf, String email, Endereco endereco) {
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
     }
 
     // MÉTODOS
-    public int getId(){
-        return this.id;
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public String getNome(){
-        return this.nome;
+    public String getNome() {
+        return nome;
     }
 
-    public void setNome(String nome){
+    public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getCpf(){
-        return this.cpf;
+    public String getCpf() {
+        return cpf;
     }
 
-    public void setCpf(String cpf){
+    public void setCpf(String cpf) {
         this.cpf = cpf;
     }
 
@@ -66,5 +76,13 @@ public class Usuario {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
     }
 }
