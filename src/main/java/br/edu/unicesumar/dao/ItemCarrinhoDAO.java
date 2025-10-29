1d5f489349174851661dad7da0a0636181579453
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-28 23:19:17 -0300

    Finalizado o arquivo DAO da classe ItemCarrinho

---
+++ src/main/java/br/edu/unicesumar/dao/ItemCarrinhoDAO.java
@@ -0,0 +1,33 @@
+package br.edu.unicesumar.dao;
+
+import java.util.List;
+
+import br.edu.unicesumar.model.ItemCarrinho;
+import jakarta.persistence.TypedQuery;
+
+public class ItemCarrinhoDAO extends DAO<ItemCarrinho>{
+
+    //LISTANDO TODOS OS ITENS DO CARRINHO
+    public List<ItemCarrinho> listAllCartItens(){
+        try {
+            TypedQuery<ItemCarrinho> query = em.createQuery("SELECT ic FROM ItemCarrinho ic", ItemCarrinho.class);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            return List.of();
+        }
+    }
+
+    //LISTANDO O ITEM DO CARRINHO PELO IDENTIFICADOR
+    public ItemCarrinho findCartItemById(int id){
+        try {
+            TypedQuery<ItemCarrinho> query = em.createQuery("SELECT ic FROM ItemCarrinho ic " + "WHERE ic.id = :id", ItemCarrinho.class);
+
+            query.setParameter("id", id);
+
+            return query.getSingleResult();      
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
