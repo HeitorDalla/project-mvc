d4e7daf2738ca80b71455969a61b77f5e12e85f5
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-28 21:02:17 -0300

    Finalizado o arquivo DAO da classe Pedido

---
+++ src/main/java/br/edu/unicesumar/dao/PedidoDAO.java
@@ -0,0 +1,33 @@
+package br.edu.unicesumar.dao;
+
+import java.util.List;
+
+import br.edu.unicesumar.model.Pedido;
+import jakarta.persistence.TypedQuery;
+
+public class PedidoDAO extends DAO<Pedido>{
+
+    //LISTANDO TODOS OS PEDIDOS
+    public List<Pedido> listAllOrders(){
+        try {
+            TypedQuery<Pedido> query = em.createQuery("SELECT p FROM Pedido p", Pedido.class);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    //LISTANDO O PEDIDO PELO IDENTIFICADOR
+    public Pedido findOrderById(int id){
+        try {
+            TypedQuery<Pedido> query = em.createQuery("SELECT p FROM Pedido p " + "WHERE p.id = :id", Pedido.class);
+
+            query.setParameter("id", id);
+
+            return query.getSingleResult();      
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
