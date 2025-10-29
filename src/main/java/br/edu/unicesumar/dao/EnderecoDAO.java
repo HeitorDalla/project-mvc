92f1194a729155d9f06438bf07cf5261e21b5e57
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-28 21:00:39 -0300

    Finalizado o arquivo DAO do Endereco

---
+++ src/main/java/br/edu/unicesumar/dao/EnderecoDAO.java
@@ -0,0 +1,33 @@
+package br.edu.unicesumar.dao;
+
+import java.util.List;
+
+import br.edu.unicesumar.model.Endereco;
+import jakarta.persistence.TypedQuery;
+
+public class EnderecoDAO extends DAO<Endereco>{
+
+    //LISTANDO TODOS OS ENDERECOS
+    public List<Endereco> listAllAddress(){
+        try {
+            TypedQuery<Endereco> query = em.createQuery("SELECT e FROM Endereco e", Endereco.class);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    //LISTANDO O ENDERECO PELO IDENTIFICADOR
+    public Endereco findAddressById(int id){
+        try {
+            TypedQuery<Endereco> query = em.createQuery("SELECT e FROM Endereco e " + "WHERE e.id = :id", Endereco.class);
+
+            query.setParameter("id", id);
+
+            return query.getSingleResult();      
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
