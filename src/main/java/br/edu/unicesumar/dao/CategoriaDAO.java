d58fbd3f312058cb6748cd8b6890a81636d7c230
HeitorDalla <heitorvillavilla@gmail.com>
2025-10-28 23:18:45 -0300

    Finalizado o arquivo DAO da classe Categoria

---
+++ src/main/java/br/edu/unicesumar/dao/CategoriaDAO.java
@@ -0,0 +1,33 @@
+package br.edu.unicesumar.dao;
+
+import java.util.List;
+
+import br.edu.unicesumar.model.Categoria;
+import jakarta.persistence.TypedQuery;
+
+public class CategoriaDAO extends DAO<Categoria>{
+
+    //LISTANDO TODOS AS CATEGORIAS
+    public List<Categoria> listAllCategories(){
+        try {
+            TypedQuery<Categoria> query = em.createQuery("SELECT c FROM Categoria c", Categoria.class);
+
+            return query.getResultList();
+        } catch (Exception e) {
+            return List.of();
+        }
+    }
+
+    //LISTANDO O CATEGORIA PELO IDENTIFICADOR
+    public Categoria findCategoryById(int id){
+        try {
+            TypedQuery<Categoria> query = em.createQuery("SELECT c FROM Categoria c " + "WHERE c.id = :id", Categoria.class);
+
+            query.setParameter("id", id);
+
+            return query.getSingleResult();      
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
