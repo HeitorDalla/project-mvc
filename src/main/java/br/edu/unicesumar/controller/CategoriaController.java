0b0c23b53e5ef1ee653917abfa329ad10167c5f0
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-07 22:11:28 -0300

    feature: finalizando o controller da Categoria

---
+++ src/main/java/br/edu/unicesumar/controller/CategoriaController.java
@@ -0,0 +1,24 @@
+package br.edu.unicesumar.controller;
+
+import br.edu.unicesumar.service.CategoriaService;
+import br.edu.unicesumar.model.Categoria;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CategoriaController {
+    /* ligacao entre a view e controller */
+    @FXML
+    private Label label;
+
+    CategoriaService categoriaService = new CategoriaService();
+
+    @FXML
+    public void saveCategoria () {
+        Categoria c = new Categoria();
+        
+        c.setNome("Eletrônicos");
+
+        categoriaService.saveCategoria(c);
+    }
+}
