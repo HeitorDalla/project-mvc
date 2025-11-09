f8625d8235f982baa0c3325f1e73e8850af6d00c
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-09 17:17:51 -0300

    feat: criando o pedidoService

--- src/main/java/br/edu/unicesumar/service/PedidoService.java
+++ src/main/java/br/edu/unicesumar/service/PedidoService.java
@@ -1,43 +1,68 @@
 package br.edu.unicesumar.service;
 
 import br.edu.unicesumar.dao.PedidoDAO;
+import br.edu.unicesumar.model.ItemPedido;
 import br.edu.unicesumar.model.Pedido;
-import br.edu.unicesumar.model.Usuario;
-import br.edu.unicesumar.utils.Utils;
 
 public class PedidoService {
-    // Atributo que vai armazenar o pedidoDAO
-    private PedidoDAO pedidoDAO;
+    private final PedidoDAO pedidoDAO;
+    private final UsuarioService usuarioService;
 
     // Construtor do pedido
-    public PedidoService () {
+    public PedidoService (PedidoDAO pedidoDAO, UsuarioService usuarioService) {
         /* Instancia o pedidoDAO, que vai ser responsabilizado por chamar o banco de dados */
-        this.pedidoDAO = new PedidoDAO();
+        this.pedidoDAO = pedidoDAO;
+        this.usuarioService = usuarioService;
     }
 
+    // Salva o pedido e envia para o DAO armazenar/validar no banco de dados
     public boolean savePedido (Pedido pedido) {
-        // Apenas verifica se o objeto Usuario não é nulo, não verifica campo algum do objeto
-        if (pedido.getUsuario() == null) {
-            return false;
-        }
-
-        Usuario usuario = pedido.getUsuario();
-
-        // Verificar cada campo do objeto usuario
-        if (usuario.getNome() == null || usuario.getNome().isEmpty()) { // verificar nome nulo ou vazio
-            return false;
-        }
-
-        if (usuario.getCpf() == null || !Utils.validarCPF(usuario.getCpf())) {
-            return false;
-        }
-
-        if (usuario.getEmail() == null || usuario.getEmail().isEmpty()) {
+        if (!validarPedido(pedido)) {
             return false;
         }
 
         // Se todos as validações forem corretas, ele chama o DAO para salvar no banco de dados
         pedidoDAO.save(pedido);
+
+        return true;
+    }
+
+    // Adiciona o itemPedido ao pedido
+    public boolean addItemPedido (Pedido pedido, ItemPedido itemPedido) {
+        // Validar item antes de adicionar
+        if (!validarItemPedido(itemPedido)) {
+            return false;
+        }
+        
+        pedido.addItemPedido(itemPedido);
+
+        return true;
+    }
+
+    // Valida o pedido antes de salvar no banco de dados (DAO)
+    public boolean validarPedido (Pedido pedido) {
+        // Apenas verifica se o objeto Usuario não é nulo, não verifica campo algum do objeto
+        if (pedido.getUsuario() == null) {
+            return false;
+        }
+
+        // Chame o UsuarioService para validar o Usuario completo
+        if (!usuarioService.validarUsuario(pedido.getUsuario())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    // Valida as informacoes do item antes de adicionar ao pedido
+    public boolean validarItemPedido (ItemPedido itemPedido) {
+        if (itemPedido.getProduto() == null) {
+            return false;
+        }
+        
+        if (itemPedido.getQuantidade() <=0) {
+            return false;
+        }
 
         return true;
     }
