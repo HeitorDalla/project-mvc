e81d5c98e80c40ee10f2c6d52dc14727d0e5a6e4
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-10 15:34:34 -0300

    reafatoracao: excluindo a injecao de dependencia, adicionando dois métodos para ligacao ao DAO e mudando a logica de validacao do produto ja existente

--- src/main/java/br/edu/unicesumar/service/ItemCarrinhoService.java
+++ src/main/java/br/edu/unicesumar/service/ItemCarrinhoService.java
@@ -1,17 +1,19 @@
 package br.edu.unicesumar.service;
 
+import java.util.List;
+
 import br.edu.unicesumar.dao.ItemCarrinhoDAO;
 
 import br.edu.unicesumar.model.ItemCarrinho;
+import br.edu.unicesumar.model.Produto;
 
 public class ItemCarrinhoService {
-    // Injeção de Dependência
-    private final ItemCarrinhoDAO itemCarrinhoDAO;
-    private final ProdutoService produtoService;
+    private ItemCarrinhoDAO itemCarrinhoDAO;
+    private ProdutoService produtoService;
 
-    public ItemCarrinhoService (ItemCarrinhoDAO itemCarrinhoDAO, ProdutoService produtoService) {
-        this.itemCarrinhoDAO = itemCarrinhoDAO;
-        this.produtoService = produtoService;
+    public ItemCarrinhoService () {
+        this.itemCarrinhoDAO = new ItemCarrinhoDAO();
+        this.produtoService = new ProdutoService();
     }
 
     // Método que salvar/validar no banco de dados
@@ -27,12 +29,17 @@ public class ItemCarrinhoService {
 
     // Validar o ItemCarrinho antes de salvar no banco de dados
     public boolean validarItemCarrinho (ItemCarrinho itemCarrinho) {
+        if (itemCarrinho == null) {
+            return false;
+        }
+
         if (itemCarrinho.getProduto() == null) {
             return false;
         }
 
-        // Chame o ProdutoService para validar o produto completo
-        if (!produtoService.validarProduto(itemCarrinho.getProduto())) {
+        // Verificar se o produto ja existe no banco de dados
+        Produto produto = produtoService.findById(itemCarrinho.getProduto().getId());
+        if (produto == null) {
             return false;
         }
 
@@ -41,5 +48,15 @@ public class ItemCarrinhoService {
         }
 
         return true;
+    }
+
+    // Recebe o id do Controller, busca o ItemCarrinho no DAO e retorna o resultado ao Controller
+    public ItemCarrinho findById(int id) {
+        return itemCarrinhoDAO.findById(id);
+    }
+
+    // Método para listar todos os Usuários
+    public List<ItemCarrinho> listAll () {
+        return itemCarrinhoDAO.listAll();
     }
 }
