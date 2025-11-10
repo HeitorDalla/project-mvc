094edfaf29ee2d80a5eaf3ec64e59e19c3bdb6a8
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-10 09:35:17 -0300

    feat: adicionando o controller de Categoria e Produto

--- src/main/java/br/edu/unicesumar/controller/CategoriaController.java
+++ src/main/java/br/edu/unicesumar/controller/CategoriaController.java
@@ -1,23 +1,33 @@
 package br.edu.unicesumar.controller;
 
 import br.edu.unicesumar.service.CategoriaService;
+
 import br.edu.unicesumar.model.Categoria;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class CategoriaController {
+    // Injeção de Dependência
+    private final CategoriaService categoriaService;
+    
+    public CategoriaController(CategoriaService categoriaService) {
+        this.categoriaService = categoriaService;
+    }
+
     /* ligacao entre a view e controller */
     @FXML
     private Label label;
-
-    CategoriaService categoriaService = new CategoriaService();
 
     @FXML
     public void saveCategoria () {
         Categoria c = new Categoria();
         c.setNome("Eletrônicos");
 
+        // Salva tudo de uma vez
         categoriaService.saveCategoria(c);
+
+        // Mensagem de conclusão
+        label.setText("Categoria adicionada com sucesso!");
     }
 }
--- src/main/java/br/edu/unicesumar/controller/ProdutoController.java
+++ src/main/java/br/edu/unicesumar/controller/ProdutoController.java
@@ -1,6 +1,7 @@
 package br.edu.unicesumar.controller;
 
 import br.edu.unicesumar.service.ProdutoService;
+
 import br.edu.unicesumar.model.Produto;
 import br.edu.unicesumar.model.Categoria;
 
@@ -8,11 +9,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class ProdutoController {
+    // Injeção de Dependência
+    private final ProdutoService produtoService;
+
+    public ProdutoController(ProdutoService produtoService) {
+        this.produtoService = produtoService;
+    }
+
     /* ligacao entre a view e controller */
     @FXML
     private Label label;
-
-    ProdutoService produtoService = new ProdutoService();
 
     @FXML
     public void saveProduto () {
@@ -25,6 +31,14 @@ public class ProdutoController {
         p.setPreco(10);
         p.setCategoria(c); // adicionando a categoria ao produto
 
-        produtoService.saveProduto(p);
+        // Salva tudo de uma vez
+        boolean sucesso = produtoService.saveProduto(p);
+
+        // Mensagem de conclusão
+        if (sucesso) {
+            label.setText("Produto adicionado com sucesso!");
+        } else {
+            label.setText("Erro ao salvar o produto. Verifique os dados!");
+        }
     }
 }
