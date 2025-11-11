f857f19dd324b0802701be0715c9a6d633d708d8
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-11 13:23:49 -0300

    refatoracao: adicionado a logica de tratamento de exceções

--- src/main/java/br/edu/unicesumar/controller/ProdutoController.java
+++ src/main/java/br/edu/unicesumar/controller/ProdutoController.java
@@ -3,6 +3,7 @@ package br.edu.unicesumar.controller;
 import br.edu.unicesumar.service.ProdutoService;
 
 import br.edu.unicesumar.model.Produto;
+import br.edu.unicesumar.exception.BusinessException;
 import br.edu.unicesumar.model.Categoria;
 
 import javafx.fxml.FXML;
@@ -21,22 +22,25 @@ public class ProdutoController {
 
     @FXML
     public void saveProduto () {
-        Categoria c = new Categoria();
-        c.setNome("Comida");
+        try {
+            Categoria c = new Categoria();
+            c.setNome("Comida");
 
-        Produto p = new Produto();
-        p.setNome("Limao");
-        p.setDescricao("Bom para salada");
-        p.setPreco(10);
-        p.setCategoria(c); // adicionando a categoria ao produto
+            Produto p = new Produto();
+            p.setNome("Limao");
+            p.setDescricao("Bom para salada");
+            p.setPreco(10);
+            p.setCategoria(c); // adicionando a categoria ao produto
 
-        // Salva tudo de uma vez
-        boolean sucesso = produtoService.saveProduto(p);
+            // Salva tudo de uma vez
+            produtoService.saveProduto(p);
 
-        // Mensagem de conclusão
-        if (sucesso) {
             label.setText("Produto adicionado com sucesso!");
-        } else {
+        } catch (BusinessException ex) {
+            label.setText(ex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+
             label.setText("Erro ao salvar o produto. Verifique os dados!");
         }
     }
