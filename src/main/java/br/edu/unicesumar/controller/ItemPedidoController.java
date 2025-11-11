9e21872228b3c6f57efc5386095070be96348c1c
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-11 13:24:08 -0300

    refatoracao: adicionado a logica de tratamento de exceções

--- src/main/java/br/edu/unicesumar/controller/ItemPedidoController.java
+++ src/main/java/br/edu/unicesumar/controller/ItemPedidoController.java
@@ -3,7 +3,7 @@ package br.edu.unicesumar.controller;
 import br.edu.unicesumar.service.ItemPedidoService;
 import br.edu.unicesumar.service.PedidoService;
 import br.edu.unicesumar.service.ProdutoService;
-
+import br.edu.unicesumar.exception.BusinessException;
 import br.edu.unicesumar.model.ItemPedido;
 import br.edu.unicesumar.model.Pedido;
 import br.edu.unicesumar.model.Produto;
@@ -28,38 +28,41 @@ public class ItemPedidoController {
 
     @FXML
     public void saveItemPedido () {
-        // Vai vir da UI (exemplo: 1)
-        int idPedido = 1;
-        int quantidadeSelecionada = 1;
-        
-        // Controller - Service
-        Pedido pedido = pedidoService.findById(idPedido);
-        if (pedido == null) {
-            label.setText("Pedido não encontrado!");
-            return;
-        }
+        try {
+            // Vai vir da UI (exemplo: 1)
+            int idPedido = 1;
+            int quantidadeSelecionada = 1;
+            
+            // Controller - Service
+            Pedido pedido = pedidoService.findById(idPedido);
+            if (pedido == null) {
+                label.setText("Pedido não encontrado!");
+                return;
+            }
 
-        // Vai vir da UI (exemplo: 1)
-        Produto produto = produtoService.findById(1);
-        if (produto == null) {
-            label.setText("produto não encontrado!");
-            return;
-        }
+            // Vai vir da UI (exemplo: 1)
+            Produto produto = produtoService.findById(1);
+            if (produto == null) {
+                label.setText("Produto não encontrado!");
+                return;
+            }
 
-        ItemPedido ip = new ItemPedido();
-        ip.setProduto(produto);
-        ip.setQuantidade(quantidadeSelecionada);
+            ItemPedido ip = new ItemPedido();
+            ip.setProduto(produto);
+            ip.setQuantidade(quantidadeSelecionada);
 
-        // Adiciona o item ao pedido - ja calcula o valor total e set o pedido no item
-        pedido.addItemPedido(ip);
+            // Adiciona o item ao pedido - ja calcula o valor total e set o pedido no item
+            pedido.addItemPedido(ip);
 
-        // Salva tudo de uma vez
-        boolean sucesso = itemPedidoService.saveItemPedido(pedido, ip);
+            // Salva tudo de uma vez
+            itemPedidoService.saveItemPedido(pedido, ip);
 
-        // Mensagem de conclusão
-        if (sucesso) {
             label.setText("Item adicionado ao pedido salvo com sucesso!");
-        } else {
+        } catch (BusinessException ex) {
+            label.setText(ex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+
             label.setText("Erro ao tentar salvar item no carrinho. Verifique os dados!");
         }
     }
