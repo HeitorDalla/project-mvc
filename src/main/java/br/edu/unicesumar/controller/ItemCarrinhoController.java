1fa96a91b4b284dda179fec7a0091609278da69d
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-11 13:24:17 -0300

    refatoracao: adicionado a logica de tratamento de exceções

--- src/main/java/br/edu/unicesumar/controller/ItemCarrinhoController.java
+++ src/main/java/br/edu/unicesumar/controller/ItemCarrinhoController.java
@@ -2,7 +2,7 @@ package br.edu.unicesumar.controller;
 
 import br.edu.unicesumar.service.ItemCarrinhoService;
 import br.edu.unicesumar.service.ProdutoService;
-
+import br.edu.unicesumar.exception.BusinessException;
 import br.edu.unicesumar.model.ItemCarrinho;
 import br.edu.unicesumar.model.Produto;
 
@@ -24,26 +24,29 @@ public class ItemCarrinhoController {
 
     @FXML
     public void saveItemCarrinho () {
-        // Vai vir da UI
-        int idProdutoSelecionado = 1;
-        int quantidadeSelecionada = 2;
+        try {
+            // Vai vir da UI
+            int idProdutoSelecionado = 1;
+            int quantidadeSelecionada = 2;
 
-        Produto produto = produtoService.findById(idProdutoSelecionado);
-        if (produto == null) {
-            return;
-        }
+            Produto produto = produtoService.findById(idProdutoSelecionado);
+            if (produto == null) {
+                return;
+            }
 
-        ItemCarrinho ic = new ItemCarrinho();
-        ic.setProduto(produto);
-        ic.setQuantidade(quantidadeSelecionada);
-        
-        // Salva tudo de uma vez
-        boolean sucesso = itemCarrinhoService.saveItemCarrinho(ic);
+            ItemCarrinho ic = new ItemCarrinho();
+            ic.setProduto(produto);
+            ic.setQuantidade(quantidadeSelecionada);
+            
+            // Salva tudo de uma vez
+            itemCarrinhoService.saveItemCarrinho(ic);
 
-        // Mensagem de conclusão
-        if (sucesso) {
             label.setText("Item adicionada ao carrinho com sucesso!");
-        } else {
+        } catch (BusinessException ex) {
+            label.setText(ex.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace(); // vai mostrar qual a pilha inteira, ou seja, qual método esta chamando qual método
+
             label.setText("Erro ao tentar salvar item no carrinho. Verifique os dados!");
         }
     }
